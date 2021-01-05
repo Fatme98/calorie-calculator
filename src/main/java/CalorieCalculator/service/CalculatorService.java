ca1d4383af607b9d339ba9816c89b1d46c408849
Fatme98 <69259691+Fatme98@users.noreply.github.com>
2021-01-05 10:38:40 +0200

    Delete CalculatorService.java

--- src/main/java/CalorieCalculator/service/CalculatorService.java
+++
@@ -1,9 +0,0 @@
-package CalorieCalculator.service;
-
-import CalorieCalculator.model.service.CalculatorServiceModel;
-import CalorieCalculator.model.view.CalculatorViewModel;
-
-public interface CalculatorService {
-    CalculatorViewModel calculateCalories(CalculatorServiceModel calculatorServiceModel, String username);
-
-}

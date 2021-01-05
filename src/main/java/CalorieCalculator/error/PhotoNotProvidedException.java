aae0423f9fd09604e46eb4cb6e06952893b400fb
Fatme98 <69259691+Fatme98@users.noreply.github.com>
2021-01-05 10:35:01 +0200

    Delete PhotoNotProvidedException.java

--- src/main/java/CalorieCalculator/error/PhotoNotProvidedException.java
+++
@@ -1,8 +0,0 @@
-package CalorieCalculator.error;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code= HttpStatus.NOT_FOUND, reason = "Photo not provided!")
-public class PhotoNotProvidedException extends RuntimeException{
-}
